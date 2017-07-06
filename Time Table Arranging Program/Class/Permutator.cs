1bd8743b47df6a5e74c6ffea03f8387164ae830e
Wong Jia Hau <hou32hou@gmail.com>
2017-07-06 23:05:47 +0800

    Added permutator for BinarizedSlot

--- Time Table Arranging Program/Class/Permutator.cs
+++ Time Table Arranging Program/Class/Permutator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Time_Table_Arranging_Program.Class;
 using Time_Table_Arranging_Program.Class.Converter;
+using Time_Table_Arranging_Program.Model;
 
 namespace Time_Table_Arranging_Program {
     public static class Permutator {
@@ -37,7 +38,7 @@ namespace Time_Table_Arranging_Program {
             final:
             return result;
         }
-
+        
         public static List<List<Slot>> Run_v2_withoutConsideringWeekNumber(Slot[] input) {
             if (input == null || input.Length == 0) return null;
             var result = new List<List<Slot>>();
@@ -165,6 +166,114 @@ namespace Time_Table_Arranging_Program {
         }
 
         public static List<List<T>> Partitionize<T>(T[] input) where T : Slot {
+            var result = new List<List<T>>();
+            var column = new List<T>();
+            var copy = input.ToList();
+            while (copy.Count > 0) {
+                column.Add(copy[0]);
+                var i = 1;
+                while (i < copy.Count) {
+                    for (var j = 0 ; j < column.Count ; j++) {
+                        if (copy[i].Code != column[j].Code || copy[i].Type != column[j].Type ||
+                            copy[i].Number == column[j].Number) {
+                            i++;
+                            goto there;
+                        }
+                    }
+                    column.Add(copy[i]);
+                    copy.RemoveAt(i);
+
+                    there:
+                    ;
+                }
+                result.Add(new List<T>(column));
+                column.Clear();
+                copy.RemoveAt(0);
+            }
+            return result;
+        }
+
+        public struct KeyValuePair {
+            public readonly int Key;
+            public readonly int Value;
+
+            public KeyValuePair(int key , int value) {
+                Key = key;
+                Value = value;
+            }
+        }
+    }
+
+    public static class PermutatorForBinarizedSlot {        
+        public static List<List<BinarizeSlotModel>> Run_v2_WithConsideringWeekNumber(BinarizeSlotModel[] input) {
+            if (input == null || input.Length == 0) return null;
+            var result = new List<List<BinarizeSlotModel>>();
+            var partitioned = Partitionize(input);
+            var indices = GenerateIndices(partitioned);
+            var prototype = new List<BinarizeSlotModel>(partitioned.Count);
+
+            var crashedIndexList = new List<KeyValuePair[]>();
+            while (true) {
+                prototype.Add(partitioned[0][indices[0].Value]);
+                for (var i = 1 ; i < indices.Count ; i++) {
+                    var current = partitioned[i][indices[i].Value];
+                    for (var j = prototype.Count - 1 ; j >= 0 ; j--) {
+                        if (!prototype[j].IntersectWith(current)) continue;
+                        crashedIndexList.Add(new KeyValuePair[2]
+                            {new KeyValuePair(j, indices[j].Value), new KeyValuePair(i, indices[i].Value)});
+                        goto here;
+                    }
+                    prototype.Add(current);
+                }
+                result.Add(new List<BinarizeSlotModel>(prototype));
+                here:
+                do {
+                    indices = Increment(indices);
+                    if (indices == null) goto final;
+                } while (crashedIndexList.Any(x => ContainCrashedIndex(indices , x)));
+                prototype.Clear();
+            }
+            final:
+            return result;
+        }
+
+
+        private static bool ContainCrashedIndex(List<BoundedInt> indices , KeyValuePair[] crashedIndex) {
+            if (indices[crashedIndex[0].Key].Value == crashedIndex[0].Value
+                &&
+                indices[crashedIndex[1].Key].Value == crashedIndex[1].Value
+            ) return true;
+            return false;
+        }
+
+
+        public static List<BoundedInt> Increment(List<BoundedInt> indices) {
+            var pointer = indices.Count - 1;
+            var x = new List<BoundedInt>(indices);
+            while (true) {
+                x[pointer].Value++;
+                if (x[pointer].Value > x[pointer].UpperLimit) {
+                    x[pointer].Value = 0;
+                    pointer--;
+                    if (pointer < 0) return null;
+                }
+                else return x;
+            }
+        }
+
+        public static List<BoundedInt> GenerateIndices<T>(List<List<T>> x) {
+            var result = new List<BoundedInt>();
+            var bi = new BoundedInt();
+            bi.Value = 0;
+            for (var i = 0 ; i < x.Count ; i++) {
+                bi.UpperLimit = x[i].Count - 1;
+                result.Add(new BoundedInt(bi));
+            }
+
+            return result;
+        }
+
+        public static List<List<T>> Partitionize<T>(T[] input) where T : BinarizeSlotModel {
             var result = new List<List<T>>();
             var column = new List<T>();
             var copy = input.ToList();
--- Time Table Arranging Program/Model/BinarizeSlotModel.cs
+++ Time Table Arranging Program/Model/BinarizeSlotModel.cs
@@ -10,14 +10,29 @@ namespace Time_Table_Arranging_Program.Model {
         private int _day;
         private int _timePeriod;
         private int _weekNumber;
+        private string _code;
+        private string _type;
+        private int _number;
+        public string Code => _code;
+        public string Type => _type;
+        public int Number => _number;
         public BinarizeSlotModel(Slot s) {
             _day = s.Day.ToBinary();
             _timePeriod = s.TimePeriod.ToBinary();
             _weekNumber = s.WeekNumber.ToBinary();
+            _code = s.Code;
+            _type = s.Type;
+            _number = int.Parse(s.Number);
         }
 
         public bool IntersectWith(BinarizeSlotModel other) {
-            throw new NotImplementedException();
+            if (_code == other._code && _type == other._type && _number != other._number) return true;
+            if ((_day & other._day) == 0) return false;
+            if((_timePeriod & other._timePeriod)==0) return false;
+            return (_weekNumber & other._weekNumber) > 0;
+
         }
+
+
     }
 }
