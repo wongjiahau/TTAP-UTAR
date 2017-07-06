3d9001cf4aa3f20b2205ed95f39b6d87b8d1ed58
Wong Jia Hau <hou32hou@gmail.com>
2017-07-06 12:12:04 +0800

    Added SubjectModel.cs

---
+++ Time Table Arranging Program/Model/SubjectModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time_Table_Arranging_Program.Model
+{
+    public class SubjectModel
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public int CreditHour { get; set; }
+
+    }
+}
