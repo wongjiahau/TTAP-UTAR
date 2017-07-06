8b29c97129d6a4e336e7818182dc91867ad97b71
Wong Jia Hau <hou32hou@gmail.com>
2017-07-06 12:26:56 +0800

    Added constructor for SubjectModel

--- Time Table Arranging Program/Model/SubjectModel.cs
+++ Time Table Arranging Program/Model/SubjectModel.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Time_Table_Arranging_Program.Class;
 
 namespace Time_Table_Arranging_Program.Model
 {
     public class SubjectModel
     {
-        public string Name { get; set; }
-        public string Code { get; set; }
-        public int CreditHour { get; set; }
+        public SubjectModel(string name, string code, int creditHour, List<Slot> slots)
+        {
+            Name = name;
+            Code = code;
+            CreditHour = creditHour;
+            Slots = slots;
+        }
 
+        public string Name { get; private set; }
+        public string Code { get; private set; }
+        public int CreditHour { get; private set; }
+
+        public List<Slot> Slots { get; private set; }
     }
 }
