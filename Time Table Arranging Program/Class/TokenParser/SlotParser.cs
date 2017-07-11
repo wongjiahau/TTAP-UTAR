660181426489083cdb7851087693d403d9b10fc7
Wong Jia Hau <hou32hou@gmail.com>
2017-07-11 11:01:53 +0800

    Removed an unused method in SlotParser.cs

--- Time Table Arranging Program/Class/TokenParser/SlotParser.cs
+++ Time Table Arranging Program/Class/TokenParser/SlotParser.cs
@@ -44,21 +44,6 @@ namespace Time_Table_Arranging_Program.Class.TokenParser {
         }
 
 
-        private string Beautify(string s) {
-            var tokens = s.Split(' ');
-            for (var i = 0; i < tokens.Length; i++) {
-                if (tokens[i].All(t => t == 'I')) continue;
-                if (tokens[i] == "") continue;
-                tokens[i] = tokens[i].ToLower();
-                tokens[i] = char.ToUpper(tokens[i][0]) + tokens[i].Substring(1, tokens[i].Length - 1);
-            }
-            var result = "";
-            foreach (var t in tokens) {
-                result += t + " ";
-            }
-            return result.Substring(0, result.Length - 1);
-        }
-
         private bool TryParseSubjectCode(ITokenStream ts, ref Slot resultSlot) {
             if (ts.CurrentToken().IsPossiblySubjectCode() && ts.NextToken().Value() == "-") {
                 resultSlot.Code = ts.CurrentToken().Value();
