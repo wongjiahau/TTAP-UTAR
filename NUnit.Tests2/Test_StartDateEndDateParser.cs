ac19c37219b925c0d5ac2d2f5990c29a285480b2
Wong Jia Hau <hou32hou@gmail.com>
2017-07-12 11:52:24 +0800

    Rename a test

--- NUnit.Tests2/Test_StartDateEndDateParser.cs
+++ NUnit.Tests2/Test_StartDateEndDateParser.cs
@@ -6,7 +6,7 @@ using Time_Table_Arranging_Program.Class;
 
 namespace NUnit.Tests2 {
     [TestFixture]
-    public class Test_StartTimeEndTimeParser {
+    public class Test_StartDateEndDateParser {
         private static string input =
      "Home\t\t\r\n   Log Out  \t\r\n \r\nWelcome, LOW KE LI (15UKB04769)   User Guide\r\n\r\n Course Timetable Preview\r\n\r\n\tCourse Timetable Preview\t\tMy Course Registration\t\r\n\r\n\r\nSESSION\t201705\tCLASS TYPE\tFull-time\tFACULTY\tFAM\tCAMPUS\tSungai Long Campus\tDURATION (WEEKS)\t29/05/2017 - 03/09/2017 (14)\r\n\r\nCOURSE\t\r\n  Search\r\nDAY\t";
 
