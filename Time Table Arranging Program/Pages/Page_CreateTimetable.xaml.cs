a375e4edde3c7d18553e1a8d45107f7a218a1106
wongjiahau <hou32hou@gmail.com>
2017-07-14 11:33:06 +0800

    Feedback to show "how many possible timetables is found" is allowed again.

--- Time Table Arranging Program/Pages/Page_CreateTimetable.xaml.cs
+++ Time Table Arranging Program/Pages/Page_CreateTimetable.xaml.cs
@@ -86,7 +86,7 @@ namespace Time_Table_Arranging_Program.Pages {
                 ToolBoxPanel.Visibility = Visibility.Visible;
                 _cyclicIndex.MaxValue = result.Count - 1;
                 _cyclicIndex.CurrentValue = 0;
-                //AutoClosePopup.Show(result.Count + " possible timetables found.");
+                AutoClosePopup.Show(result.Count + " possible timetables found.");
             }
             TimetableViewer.Initialize(_cyclicIndex);
             CyclicIndexView.DataContext = new CyclicIndexVM(_cyclicIndex);
