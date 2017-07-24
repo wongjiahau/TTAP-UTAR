ba608dd5c3038c68ec5676990dff0fb5c41a8721
WJH <hou32hou@gmail.com>
2017-07-24 10:23:01 +0800

    Change the default settings - to DONT generalize slot - because lecturer dont want slots to be generalized

--- Time Table Arranging Program/Global.cs
+++ Time Table Arranging Program/Global.cs
@@ -28,7 +28,7 @@ namespace Time_Table_Arranging_Program {
                 new Setting(Windows_Control.Setting.SettingDescription.GeneralizedSlot ,
                     "Generalize slots" ,
                     "It means to generalize slots that share the same Day, same Time, same Name and same Type as one slot" ,
-                    true);
+                    false);
         }
         public static class State {
             public static bool FileIsSavedBefore = false;
