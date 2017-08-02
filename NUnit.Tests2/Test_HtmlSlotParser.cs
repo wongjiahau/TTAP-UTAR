9b403e0ca6bd2f86743a1c975ebaab2ea8985c2f
WJH <hou32hou@gmail.com>
2017-08-02 21:29:14 +0800

    Added test mechanism in Test_HtmlSlotParser_2

--- NUnit.Tests2/Test_HtmlSlotParser.cs
+++ NUnit.Tests2/Test_HtmlSlotParser.cs
@@ -20,11 +20,16 @@ namespace NUnit.Tests2 {
         [Test]
         public void Test_HtmlSlotParser_2() {
             string input = Helper.RawStringOfTestFile("Sample HTML.txt");
-            var result = new HtmlSlotParser().Parse(input);
+            var actual = new HtmlSlotParser().Parse(input);
             var expected = new List<Slot>()
             {
-                new Slot(1,"MPU3113", "HUBUNGAN ETNIK (FOR LOCAL STUDENTS)".Beautify(), "1", "L", Day.Monday,"KB521",new TimePeriod(Time.CreateTime_24HourFormat(9,00), Time.CreateTime_24HourFormat(12,00)),new WeekNumber(new List<int>(){1,2,3,4,5,6,7,8,9,10,11,12,13,14}) )
+                new Slot(1,"MPU3113", "HUBUNGAN ETNIK (FOR LOCAL STUDENTS)".Beautify(), "1", "L", Day.Monday,"KB521",new TimePeriod(Time.CreateTime_24HourFormat(9,00), Time.CreateTime_24HourFormat(12,00)),new WeekNumber(new List<int>(){1,2,3,4,5,6,7,8,9,10,11,12,13,14}) ),
+
+                
             };
+            for (int i = 0; i < expected.Count; i++) {
+                Assert.IsTrue(actual[i].Equals(expected[i]));
+            }
         }
     }
 }
