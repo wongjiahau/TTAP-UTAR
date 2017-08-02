467f206e64b16d148e7b78d6bab59a0c5f6b6295
c <chooweisung@gmail.com>
2017-08-02 21:32:01 +0800

    add test data 12 &13

--- NUnit.Tests2/Test_HtmlSlotParser.cs
+++ NUnit.Tests2/Test_HtmlSlotParser.cs
@@ -31,5 +31,33 @@ namespace NUnit.Tests2 {
                 Assert.IsTrue(actual[i].Equals(expected[i]));
             }
         }
+
+	    [Test]
+	    public void Test_htmlslotparser_12()
+	    {
+		    string input = Helper.RawStringOfTestFile("Sample HTML.txt");
+			var result = new HtmlSlotParser().Parse(input);
+		    var expected = new List<Slot>()
+		    {
+			    new Slot(11, "MPU32013", "BAHASA KEBANGSAAN A".Beautify(), "1", "L", Day.Tuesday, "KB314",
+				    new TimePeriod(Time.CreateTime_24HourFormat(9, 00), Time.CreateTime_24HourFormat(12, 00)),
+				    new WeekNumber(new List<int>() {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}))
+		    };
+
+	    }
+
+	    [Test]
+	    public void Test_htmlslotparser_13()
+	    {
+		    string input = Helper.RawStringOfTestFile("Sample HTML.txt");
+		    var result = new HtmlSlotParser().Parse((input));
+		    var expected = new List<Slot>()
+		    {
+			    new Slot(12, "MPU32033", "ENGLISH FOR PROFESSIONALS".Beautify(), "1", "L", Day.Monday, "KB301",
+				    new TimePeriod(Time.CreateTime_24HourFormat(12, 00), Time.CreateTime_24HourFormat(13, 00)),
+				    new WeekNumber(new List<int>() {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}))
+		    };
+
+	    }
     }
 }
