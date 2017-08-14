5550677d3ca2a1eaa07a863d75889f6f5edd4187
WJH <hou32hou@gmail.com>
2017-08-15 07:23:11 +0800

    Added classses for ConsoleTerminal

---
+++ Time Table Arranging Program/Console/ConsoleContent.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+public class ConsoleContent : INotifyPropertyChanged {
+    string _consoleInput = string.Empty;
+    ObservableCollection<string> _consoleOutput = new ObservableCollection<string>() { "Console Emulation Sample..." };
+
+    public string ConsoleInput {
+        get {
+            return _consoleInput;
+        }
+        set {
+            _consoleInput = value;
+            OnPropertyChanged("ConsoleInput");
+        }
+    }
+
+    public ObservableCollection<string> ConsoleOutput {
+        get {
+            return _consoleOutput;
+        }
+        set {
+            _consoleOutput = value;
+            OnPropertyChanged("ConsoleOutput");
+        }
+    }
+
+    public void RunCommand() {
+        ConsoleOutput.Add(ConsoleInput);
+        // do your stuff here.
+        ConsoleInput = String.Empty;
+    }
+
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    void OnPropertyChanged(string propertyName) {
+        PropertyChanged?.Invoke(this , new PropertyChangedEventArgs(propertyName));
+    }
+}
