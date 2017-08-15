5b9576c91a483068aff18589ea9db76c781d6a62
WJH <hou32hou@gmail.com>
2017-08-15 22:30:51 +0800

    Moved classes of ConsoleTerminal to the new project - so that it can be reused by another project next time

--- ConsoleTerminalLibrary/Console/ConsoleCommand.cs
+++ ConsoleTerminalLibrary/Console/ConsoleCommand.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Time_Table_Arranging_Program.Console {
+﻿namespace ConsoleTerminalLibrary.Console {
     public interface IConsoleCommand {
         void Execute();
     }
