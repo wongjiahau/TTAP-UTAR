319a9e7d4f3f88ef9dedb4da40206e825d2b2124
WJH <hou32hou@gmail.com>
2017-08-16 08:14:33 +0800

    Added some built-in command for ConsoleTerminal

---
+++ ConsoleTerminalLibrary/BuildIn_Command/ClearScreenCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleTerminalLibrary.Console;
+
+namespace ConsoleTerminalLibrary.BuildIn_Command {
+    public class ClearScreenCommand : ConsoleCommandBase {
+        public ClearScreenCommand(object commandee, string keyword, string help) : base(commandee, keyword, help) { }
+        public override string Execute() {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ConsoleTerminalLibrary/BuildIn_Command/HelpCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleTerminalLibrary.Console;
+
+namespace ConsoleTerminalLibrary.BuildIn_Command {
+    public class HelpCommand : ConsoleCommandBase {
+        public HelpCommand(object commandee) : base(commandee, "help", "Show list of available commands.") { }
+
+        public override string Execute() {
+            var commandList = Commandee as List<IConsoleCommand>;
+            string result = "Available commands :\n";
+            foreach (IConsoleCommand t in commandList) {
+                result += "\t" + t.Keyword + "\n";
+            }
+            return result;
+        }
+    }
+}
