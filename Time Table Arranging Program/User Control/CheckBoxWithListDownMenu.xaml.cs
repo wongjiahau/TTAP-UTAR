4926d0b17e09f34581ba0bdecb3b51a92d2305d9
WJH <hou32hou@gmail.com>
2017-08-28 10:08:05 +0800

    Fix an issue where height of CheckBoxWithListDownMenu is not restored. - after showing an error message then hiding it

--- Time Table Arranging Program/User Control/CheckBoxWithListDownMenu.xaml.cs
+++ Time Table Arranging Program/User Control/CheckBoxWithListDownMenu.xaml.cs
@@ -60,6 +60,7 @@ namespace Time_Table_Arranging_Program.User_Control {
             if (message == null) {
                 DrawerHost.IsRightDrawerOpen = false;
                 DrawerHost.IsEnabled = true;
+                ErrorTextBlock.Text = "";
                 if (!this.IsChecked) ChooseSlotButton.Visibility = Visibility.Hidden;
             }
             else {
