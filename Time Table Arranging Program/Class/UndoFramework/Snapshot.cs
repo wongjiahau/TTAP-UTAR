90d48a4ef1e973470c3df71891041a989445e2b9
WJH <hou32hou@gmail.com>
2017-08-29 08:48:40 +0800

    Renamed the memento pattern to "Snapshot" - easier to understand for me

--- Time Table Arranging Program/Class/UndoFramework/Snapshot.cs
+++ Time Table Arranging Program/Class/UndoFramework/Snapshot.cs
@@ -1,11 +1,9 @@
 ﻿namespace Time_Table_Arranging_Program.Class.UndoFramework {
-    public class Memento {
+    public class Snapshot {
         private object _state;
-
         public object GetState() {
             return _state;
         }
-
         private void SetState(object state) {
             _state = state;
         }
