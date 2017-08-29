382e602a1ce14040fab4a63f35595bb68d6e400e
WJH <hou32hou@gmail.com>
2017-08-29 08:37:15 +0800

    Added Memento pattern framework

---
+++ Time Table Arranging Program/Class/UndoFramework/IOriginator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time_Table_Arranging_Program.Class.UndoFramework {
+    public interface IOriginator {
+        void SetMemento(Memento m);
+        Memento CreateMemento();
+    }
+
+}
---
+++ Time Table Arranging Program/Class/UndoFramework/Memento.cs
@@ -0,0 +1,14 @@
+﻿namespace Time_Table_Arranging_Program.Class.UndoFramework {
+    public class Memento {
+        private object _state;
+
+        public object GetState() {
+            return _state;
+        }
+
+        private void SetState(object state) {
+            _state = state;
+        }
+    }
+
+}
