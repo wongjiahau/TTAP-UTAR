877d8cdce2573d86d43a316cd1628c25a16f6091
Lim Yu Hao <iamyuhao@hotmail.com>
2017-08-30 16:17:27 +0800

    Update test case to suit test data

--- NUnit.Tests2/Test_StartDateEndDateFinder.cs
+++ NUnit.Tests2/Test_StartDateEndDateFinder.cs
@@ -12,8 +12,8 @@ namespace NUnit.Tests2 {
         [Test]
         public void Test_1() {
             var parser = new StartDateEndDateFinder(input);
-            Assert.True(parser.GetStartDate() == new DateTime(2017 , 5 , 29 , 0 , 0 , 0));
-            Assert.True(parser.GetEndDate() == new DateTime(2017 , 9 , 3 , 0 , 0 , 0));
+            Assert.True(parser.GetStartDate() == new DateTime(2017 , 10 , 16 , 0 , 0 , 0));
+            Assert.True(parser.GetEndDate() == new DateTime(2017 , 12 , 3 , 0 , 0 , 0));
         }
 
       
