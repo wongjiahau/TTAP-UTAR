a2533f6589a3716accc24e186af3c7ed223d1224
WJH <hou32hou@gmail.com>
2017-09-01 22:27:32 +0800

    Implemented TypeOne and TypeTwo error

---
+++ Time Table Arranging Program/User Control/CheckboxWithListDownMenuFolder/ErrorMessageType/ClashingErrorType.cs
@@ -0,0 +1,7 @@
+﻿namespace Time_Table_Arranging_Program.User_Control.CheckboxWithListDownMenuFolder.ErrorMessageType {
+    public enum ClashingErrorType {
+        NoError, 
+        TypeOneError, //Clashing with one subject
+        TypeTwoError, //Clashing with combinations of several subjects
+    }
+}
---
+++ Time Table Arranging Program/User Control/CheckboxWithListDownMenuFolder/ErrorMessageType/TypeOneError.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Time_Table_Arranging_Program.User_Control.CheckboxWithListDownMenuFolder.ErrorMessageType {
+    /// <summary>
+    /// Interaction logic for ClashingWithOneSubjectError.xaml
+    /// </summary>
+    public partial class TypeOneError : UserControl {
+        public TypeOneError() {
+            InitializeComponent();
+        }
+
+        public TypeOneError(string nameOfCrashingCounterpart) : base() {
+            ErrorTextBlock.Text += " " + nameOfCrashingCounterpart;
+        }
+    }
+}
