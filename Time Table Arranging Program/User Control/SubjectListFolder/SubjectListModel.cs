92f66c2c6f3bd7cc4b8db8ed4002f768f8833e9e
WJH <hou32hou@gmail.com>
2017-09-03 23:25:02 +0800

    Added SubjectListModel - to move UI code to xaml from SelectSubjectPanel

---
+++ Time Table Arranging Program/User Control/SubjectListFolder/SubjectListModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time_Table_Arranging_Program.User_Control.SubjectListFolder
+{
+    class SubjectListModel
+    {
+    }
+}
