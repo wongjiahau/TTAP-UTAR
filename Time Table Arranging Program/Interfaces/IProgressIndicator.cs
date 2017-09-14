010a2aacdd3632721c63a7bf5767e1c5415cdba0
WJH <hou32hou@gmail.com>
2017-09-14 12:29:02 +0800

    Added IProgressIndicator.cs

---
+++ Time Table Arranging Program/Interfaces/IProgressIndicator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Time_Table_Arranging_Program.Interfaces {
+    public interface IProgressIndicator {
+        void Show();
+        void Hide();
+    }
+
+    public class MockProgressIndicator : IProgressIndicator {
+        private readonly string _message;
+        public MockProgressIndicator(string message) {
+            _message = message;
+        }
+        public void Show() {
+            Console.WriteLine(_message);
+        }
+
+        public void Hide() {
+            Console.WriteLine("Finished.");
+        }
+    }
+}
