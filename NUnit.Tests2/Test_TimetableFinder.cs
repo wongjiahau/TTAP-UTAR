3fa5394e784674e1a75008e5cc28b7aca964bdf4
WJH <hou32hou@gmail.com>
2017-10-01 06:21:56 +0800

    Added a failing test for TimetableFinder

--- NUnit.Tests2/Test_TimetableFinder.cs
+++ NUnit.Tests2/Test_TimetableFinder.cs
@@ -16,18 +16,41 @@ namespace NUnit.Tests2 {
             input.AddRange(TestData.GetSlotsByName(TestData.Subjects.StructuralAnalysisII));
             return input;
         }
+
+
+
         [Test]
         public void Test_TimetableFinder_SortBySlotCount_1() {
             var input = GetInput();
             var subjects = SubjectModel.Parse(input);
             var timtableFinder = new TimetableFinder();
             var result = timtableFinder.SortBySlotCount(subjects);
-            for (int i = 0; i < result.Count; i++) {
-                Console.WriteLine(result[i].Code+ " " + result[i].Slots.Count);
+            for (int i = 0 ; i < result.Count ; i++) {
+                Console.WriteLine(result[i].Code + " " + result[i].Slots.Count);
             }
+        }
 
+        [Test]
+        public void Test_Correctness_1() {
+            var input = new List<Slot>();
+            input.AddRange(TestData.Default().FindAll(x => x.UID == 28)); //Lecture of BEAM
+            input.AddRange(TestData.Default().FindAll(x => x.UID == 6));//Lecture of BKA
+            var subjects = SubjectModel.Parse(input);
+            var timtableFinder = new TimetableFinder();
+            var result = timtableFinder.GetPossibleTimetables(subjects , Permutator.Run_v2_withoutConsideringWeekNumber);
+            Assert.AreEqual(0 , result.Count);
+        }
 
-
+        [Test]
+        public void Test_Correctness_2() {
+            var input = new List<Slot>();
+            input.AddRange(TestData.Default().FindAll(x => x.UID == 28)); //Lecture of BEAM
+            input.AddRange(TestData.Default().FindAll(x => x.UID == 6));//Lecture of BKA
+            var subjects = SubjectModel.Parse(input);
+            var timtableFinder = new TimetableFinder();
+            var result = timtableFinder.GetPossibleTimetables(subjects , Permutator.Run_v2_withoutConsideringWeekNumber);
+            if (result.Count == 0) Assert.Pass();
+            Assert.AreEqual(4 , result[0].Count);
         }
     }
 }
